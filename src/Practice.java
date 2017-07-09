07b4ff269db90c60beca05ee8e17bf1ef518ce7b
Ivan <ngk-trade@mail.ru>
2017-07-09 22:17:12 +0300

    Длинна массива равна длинне массива введенного с консоли

---
+++ src/Practice.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by 1 on 07.07.2017.
+ */
+public class Practice {
+}
---
+++ src/module3/HomeWork3_2.java
@@ -0,0 +1,9 @@
+package module3;
+
+import java.util.Scanner;
+
+/**
+ * Created by 1 on 09.07.2017.
+ */
+public class HomeWork3_2 {
+}
